38d910345097e1b00adf986daff7b1f3cbc8a24b
ShadowXXII <94879756+ShadowXXII@users.noreply.github.com>
2025-08-20 10:02:31 -0400

    Create index.php

---
+++ www/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        echo "<h1>Hello PHP World</h1>";
+    ?>
+    <h2><?php echo "Hello PHP World";?> </h2>
+
+    <!-- <?php
+        $nom = "Matthias Champoux";
+        $age = 19;
+        $lettre = 'a';
+        $LouVendSonFlipper = false;
+        $prixDuFliper = 150.00;
+
+        if($age == 19){
+            echo "Age parfaite";
+        }
+        else if($age < 19){
+            echo "Trop jeune";
+        }
+        else if($age > 19){
+            echo "Trop vieux";
+        }
+        else{
+            echo "Extraterrestre";
+        }
+
+        echo "Matthias a " . $age . "ans";
+    ?> -->
+
+    <?php
+    $nom = "Matthias Champoux";
+    echo "<h1>$nom</h1>";
+
+
+    function somme(){
+        
+    }
+    ?>
+
+</body>
+</html>
